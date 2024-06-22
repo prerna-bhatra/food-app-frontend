8b1af334bdc96dc06cd51b9e14c539e1bb3fd43e
prernaorangeleague <prerna@orangeleague.in>
2024-06-22 09:26:24 +0530

    live

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,8 +2,6 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 // Create a new Axios instance
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://foodcorner.zapto.org/api", // Assuming you have set the API base URL in your environment variables
+  baseURL: "https://food-delievery-app-beta.vercel.app/api", // Assuming you have set the API base URL in your environment variables
   timeout: 10000, 
 });
-
-
--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 // frontend/src/socket.ts
 import { io } from 'socket.io-client';
 
-const socket = io('https://foodcorner.zapto.org');
+const socket = io('http://localhost:3005');
 
 socket.on('connect', () => {
     console.log('Connected to server');
